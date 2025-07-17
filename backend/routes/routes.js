1e47ce6f1cc9da3243f5bdee50efb176c12cc822
nitis chimpayak <nitis.182546@gmail.com>
2025-07-17 18:23:25 +0700

    create middleware product for validate inventory request

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import { createProduct, deleteProduct, getAllProducts, updateProduct } from '../controllers/inventory-controller.js'
+import { validateProduct } from '../middleware/inventory-validate.js'
 
 const router = express.Router()
 
 // inventory routes
 router.get('/inventory', getAllProducts)
-router.post('/inventory', createProduct)
-router.put('/inventory/:id', updateProduct)
+router.post('/inventory',validateProduct, createProduct)
+router.put('/inventory/:id',validateProduct, updateProduct)
 router.delete('/inventory/:id', deleteProduct)
 
 export default router
