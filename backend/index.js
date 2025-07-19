87ce9b9816adb77ad561805e0f33ed393b238782
nitis chimpayak <nitis.182546@gmail.com>
2025-07-19 23:08:44 +0700

    add backend origin cors

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,26 @@
 import express from 'express'
 import route from './routes/routes.js'
+import cors from 'cors'
+
+const whitelist = String(process.env.CORS_WHITELIST)
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin === whitelist) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 const app = express()
 
 app.use(express.json())
+app.use(cors(corsOptions))
 
 app.use('/api', route)
 
 const port = process.env.PORT
-
 app.listen(port, () => {
     console.log('Server is running')
 })
