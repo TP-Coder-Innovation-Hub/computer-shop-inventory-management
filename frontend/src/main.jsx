dfc61732a49358b214600131a1ff50fbbfe6f8e8
nitis chimpayak <nitis.182546@gmail.com>
2025-07-19 18:56:48 +0700

    add inventory route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import Home from './pages/Home'
+import Inventory from './pages/Inventory'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
+      <Route path="/inventory" element={<Inventory/>} />
     </Routes>
   </BrowserRouter>
 )
