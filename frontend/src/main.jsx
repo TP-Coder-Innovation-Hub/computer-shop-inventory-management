65176fbf0802994f1b21822c364db95c9ed8c420
nitis <nitis.182546@gmail.com>
2025-08-18 15:30:38 +0700

    create protected route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Inventory from './pages/Inventory'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import ProtectedRoute from './components/ProtectedRoute'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/inventory' element={<Inventory />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path='/inventory' element={<Inventory />} />
+      </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
     </Routes>
