7e93fc0908d5a611333c701b3d1f9ce1f86ddf90
nitis <nitis.182546@gmail.com>
2025-12-21 15:36:24 +0700

    add receive quantity page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import ProtectedRoute from './components/ProtectedRoute'
 import Deduct from './pages/Deduct'
+import Receive from './pages/Receive'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       <Route element={<ProtectedRoute />}>
         <Route path='/inventory' element={<Inventory />} />
         <Route path='/deduct/:id' element={<Deduct />} />
+        <Route path='/receive/:id' element={<Receive />} />
       </Route>
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
